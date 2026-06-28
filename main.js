8eb69f78e391f3eb780f906555a858a5751d4a0f
Website-Public <nz78dj42c6@privaterelay.appleid.com>
2026-06-28 10:59:40 -0600

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-(function(){
-  const links=[...document.querySelectorAll('.top-nav a')];
-  const active=links.find(a=>a.classList.contains('active'));
-  if(active){
-    const nav=document.querySelector('.top-nav');
-    requestAnimationFrame(()=>{nav.scrollLeft=active.offsetLeft-24;});
-  }
-})();
